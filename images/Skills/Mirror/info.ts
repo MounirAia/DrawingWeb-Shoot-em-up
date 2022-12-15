9c13083c115e54344ed899b4e5e00f97bd0b0c55
Mounir Aiache <aiamounirpro@gmail.com>
2022-12-15 15:40:51 -0500

    working on the mirror skills

---
+++ images/Skills/Mirror/info.ts
@@ -0,0 +1,23 @@
+// Level 1
+// List of animations:
+//// idle
+//// damaged (in phase)
+//// onHit
+//// destroyed
+//// spawning
+
+// Level 2
+// List of animations:
+//// idle
+//// damaged (in phase)
+//// onHit
+//// destroyed
+//// spawning
+
+// Level 3
+// List of animations:
+//// idle
+//// damaged (in phase and only for the mirror)
+//// onHit (only for the bubble)
+//// destroyed
+//// spawning
